6b5bdd06d5f3aa8336eec52c3ebb8ca3c58a9b09
Bruno Tetsuo <71572345+brunotetsuo@users.noreply.github.com>
2026-03-01 16:24:02 -0300

    resolucao das atividades e ajustes

--- src/main/java/com/trabalho/Main.java
+++ src/main/java/com/trabalho/Main.java
@@ -7,29 +7,55 @@ public class Main {
         Scanner ler = new Scanner(System.in);
         System.out.println("Digite qual atividade deseja executar: (1-22 | 0 Encerrar)");
         int atividade = ler.nextInt();
-        Atividades atividades = new Atividades();
 
         do {
             System.out.printf("-----INICIO DA ATIVIDADE %d-----%n", atividade);
             switch (atividade) {
-                case 1:
-                    atividades.atividade01(ler);
-                    break;
-                case 2:
-                    atividades.atividade02(ler);
-                    break;
-                case 3:
-                    atividades.atividade03(ler);
-                    break;
-                case 4:
-                    atividades.atividade04(ler);
-                    break;
-                case 5:
-                    atividades.atividade05(ler);
-                    break;
-                case 6:
-                    atividades.atividade06(ler);
-                    break;
+                case 1 -> Atividades.atividade01(ler);
+
+                case 2 -> Atividades.atividade02(ler);
+
+                case 3 -> Atividades.atividade03(ler);
+
+                case 4 -> Atividades.atividade04(ler);
+
+                case 5 -> Atividades.atividade05(ler);
+
+                case 6 -> Atividades.atividade06(ler);
+
+                case 7 -> Atividades.atividade07(ler);
+
+                case 8 -> Atividades.atividade08(ler);
+
+                case 9 -> Atividades.atividade09(ler);
+
+                case 10 -> Atividades.atividade10(ler);
+
+                case 11 -> Atividades.atividade11(ler);
+
+                case 12 -> Atividades.atividade12();
+
+                case 13 -> Atividades.atividade13();
+
+                case 14 -> Atividades.atividade14();
+
+                case 15 -> Atividades.atividade15();
+
+                case 16 -> Atividades.atividade16();
+
+                case 17 -> Atividades.atividade17(ler);
+
+                case 18 -> Atividades.atividade18(ler);
+
+                case 19 -> Atividades.atividade19();
+
+                case 20 -> Atividades.atividade20();
+
+                case 21 -> Atividades.atividade21(ler);
+
+                case 22 -> Atividades.atividade22(ler);
+
+                default -> System.out.println("Atividade não existe");
             }
             System.out.printf("------FIM DA ATIVIDADE %d-------%n", atividade);
             System.out.println();
